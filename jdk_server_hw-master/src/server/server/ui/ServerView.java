8373b0c6a1865a420013cfcb4da0bd82240ee8e6
asd <sksd@mail.ru>
2024-11-16 16:55:55 +0300

    DZ2

--- jdk_server_hw-master/src/server/server/ui/ServerView.java
+++
@@ -1,8 +0,0 @@
-package server.server.ui;
-
-import server.server.domain.ServerController;
-
-public interface ServerView {
-    void showMessage(String message);
-    void setServerController(ServerController serverController);
-}
